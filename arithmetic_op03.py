16dc3a8ad4dd8c3d9347062b1c9c8cb83e9c9f76
eldorbeknorboyev <144788922+eldorbeknorboyev@users.noreply.github.com>
2023-09-23 15:44:16 +0500

    homework

---
+++ arithmetic_op03.py
@@ -0,0 +1,7 @@
+#Create a variable called 'number' and assign it the two-digit number
+number=12
+#Find the reverse of the number and assign it to a variable called 'answer'.
+answer=21
+#Print the value of the 'answer'
+print(answer)
+
